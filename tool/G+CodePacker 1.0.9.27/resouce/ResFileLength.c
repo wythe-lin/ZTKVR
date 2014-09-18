223a1f2bf997f249d5ac4066f35ba42d3f18d5e9
T.C. Chiu <freexyz2007@gmail.com>
2014-09-18 12:35:15 +0800

    modify menu string
    (1) 自動關機 change to 自動關屏

--- tool/G+CodePacker 1.0.9.27/resouce/ResFileLength.c
+++ tool/G+CodePacker 1.0.9.27/resouce/ResFileLength.c
@@ -1,1 +1,1 @@
-const unsigned int	au32ResFileLenList[] = { 8589, 4076, 616, 116569, 23613, 46539, 41516, 56586};
+const unsigned int	au32ResFileLenList[] = { 8589, 4076, 616, 117797, 23613, 46539, 41516, 56586};
